a02b2f37650dec657abedba51ac261936bdc854c
Huỳnh Hữu Lộc <huuloc0802@gmail.com>
2025-08-01 14:05:17 +0700

    Sửa

--- backend/routes/chatbot.routes.js
+++ backend/routes/chatbot.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const chatbotHandler = require('../utils/chatbotHandler');
+const auth = require('../middlewares/auth.middleware');
+
+router.use(auth);
 
 router.post('/', chatbotHandler);
 
